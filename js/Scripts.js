6653952644c196cda3fa585b24e5be2c894e46ef
Drewmacauley <macauleyandy03@gmail.com>
2020-08-18 14:44:16 -0700

    add final html content
    Co-Authored-by: Garrett Brown <gman9mm@live.com>

--- js/Scripts.js
+++ js/Scripts.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
   $("form#personality_survey_1").submit(function (event) {
     event.preventDefault();
-    $("#stress-response").show();
+    $("#signs-response").show();
     $("input:checkbox[name=work-stress]:checked").each(function () {
       const workstressSympt = $(this).val();
       $('#stress-response').append( + "<br>");
     });
     $("#personality_survey_2").show();
-    $("input:checkbox[name= -]:checked").each(function () {
+    $("input:checkbox[name=high-stress]:checked").each(function () {
       const genstress = $(this).val();
-      $('#personality-survey').append( + "<br>");
+      $('#personality-response').append( + "<br>");
     });
     $('#personality-survey').hide();
   });
